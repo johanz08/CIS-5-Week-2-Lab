7c95d883c02671d3531428b494cbd964bcae5ee7
johanz08 <johanzuniga497@gmail.com>
2026-09-01 19:35:59 -0700

    Week 2 lab

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 
-// Lab 2 — Your Name
+// Lab 2 — Johan Zuniga
 // CIS 5 Week 02 · Program structure
 
 int main() {
-  // TODO: replace TODOs with YOUR labeled lines (5–6 total)
-  std::cout << "Name: TODO\n";
-  std::cout << "Interest: TODO\n";
-  std::cout << "CIS 5 goal: TODO\n";
-  std::cout << "Tool path: TODO\n";
-  std::cout << "Fun fact: TODO\n";
+  std::cout << "Name: Johan Zuniga\n";
+  std::cout << "Interest: I love playing guitar\n";
+  std::cout << "CIS 5 goal: Learn how to create starter programs and not feel overwhelmed by code\n";
+  std::cout << "Tool path: GitHub Codespaces\n";
+  std::cout << "Fun fact: I am the youngest of three\n";
   return 0;
 }
